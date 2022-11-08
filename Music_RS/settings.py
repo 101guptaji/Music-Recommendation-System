f83201d6d83cf41b732197bddbbf8f7141d794a1
Himanshu Gupta <101guptaji@gmail.com>
2022-11-08 19:42:16 +0530

    minor fix

--- Music_RS/settings.py
+++ Music_RS/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7$ash41os79*=(1h1p6^h7j*jf_$vu0bjpb8n4#ldtwtr0m6$^'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
